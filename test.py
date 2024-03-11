8517736118f73d43b7121ec76362da32a71ff003
nonepork <59335048+nonepork@users.noreply.github.com>
2024-03-11 18:37:37 +0800

    Added more lines

--- test.py
+++ test.py
@@ -3,12 +3,23 @@ with open("main-developer.py", "r", encoding="utf-8") as f:
 
 # lines that requires input:
 ltri = [73, 74, 75]
-#for i in ltri:
+for i in ltri:
+    data[i-1] = data[i-1].replace("''", "'test'") # very ugly, very nice xD
+
+# lines that need to remove value:
+ltntrv = [77]
+for i in ltntrv:
+    line = data[i-1].split("=")
+    data[i-1] = line[0] + " ''\n"
+
+# lines that need indent once:
+
+# lines that need indent twice:
 
 # lines that need to be deleted:
-ltntbd = [22, 31, 32, 63, 84, 85, 105, 112, 146, 152, 180, 233, 510, 596]
-for i in range(0, len(ltntbd)):
-    data.pop(ltntbd[i]-(i+1))
+#ltntbd = [22, 31, 32, 63, 84, 85, 105, 112, 146, 152, 180, 233, 510, 596]
+#for i in range(0, len(ltntbd)):
+#    data.pop(ltntbd[i]-(i+1))
 
 with open("test.txt", "w", encoding="utf-8") as f:
     for line in data:
